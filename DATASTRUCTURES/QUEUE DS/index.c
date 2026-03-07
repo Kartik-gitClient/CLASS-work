9b3ead8265f09e6c40ba5d2d14d87d4daaeaca3a
Kartik-khandelwal-git <kartikkhandelwal930@gmail.com>
2026-03-07 21:08:36 +0530

    CLASSES UPDATED AS PER 18/03/2026

---
+++ DATASTRUCTURES/QUEUE DS/index.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+#define Maxsize 5
+void insert();
+void delet();
+void display();
+int queue[Maxsize];
+int front=-1;
+int Rear=-1;
+
+int main(){
+  insert();
+  insert();
+  insert();
+  insert();
+  insert();
+  insert();
+  display();
+  delet();
+  delet();
+  display();
+  delet();
+  delet();
+  delet();
+  delet();
+
+}
+
+void insert(){
+  int data;
+  if(Rear == Maxsize - 1){
+    printf("\n Queue OVERFLOW");
+  }
+  else{
+    if(Rear == -1){
+      front=0;
+      Rear=0;
+    }else{
+      Rear++;
+    }
+    printf("\n Enter a value to insert");
+    scanf("%d", &data);
+    queue[Rear] = data;
+  }
+
+}
+
+void delet(){
+  if(front == -1){
+    printf("Queue UNDERFLOW !");
+  }
+  else{
+    {
+    if(front == Rear){
+      queue[front]='\0';
+      front=-1;
+      Rear=-1;
+    }
+    else{
+      queue[front]='\0';
+      front++;
+    }
+  }
+}
+}
+
+void display(){
+  printf("\n Elements of Queue are ");
+
+  for( int i=front ; i<=Rear ; i++){
+    printf("%d", queue[i]);
+  }
+}
